5ccbe557b8352918a6b38fc803f6ce35e715a6b5
0326829327 <trancongtinh20042004@gmail.com>
2024-03-29 16:31:52 +0700

    init project

---
+++ WebsiteBanNhacCu/js/trangchu.js
@@ -0,0 +1,7 @@
+function addCompany(img, nameCompany) {
+	var link = createLinkFilter('add', 'company', nameCompany);
+	var new_tag = `<a href=` + link + `><img src=` + img + `></a>`;
+
+	var khung_hangSanXuat = document.getElementsByClassName('companyMenu')[0];
+	khung_hangSanXuat.innerHTML += new_tag;
+}
